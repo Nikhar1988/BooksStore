ea3099241ac49235a30de026694a5c31ec100f88
Nikolay <harnik1988@gmail.com>
2021-06-14 14:47:13 +0300

    inicialisation project2

---
+++ src/components/bookstore-service-context/bookstore-service-context.js
@@ -0,0 +1,11 @@
+import React, { createContext } from 'react';
+
+const {
+    Provider: BookstoreServiceProvoder,
+    Consumer: BookstoreServiceConsumer
+} = createContext();
+
+export {
+    BookstoreServiceProvoder,
+    BookstoreServiceConsumer
+}
---
+++ src/components/bookstore-service-context/index.js
@@ -0,0 +1,9 @@
+import {
+    BookstoreServiceProvoder,
+    BookstoreServiceConsumer
+} from './bookstore-service-context';
+
+export {
+    BookstoreServiceProvoder,
+    BookstoreServiceConsumer
+}
