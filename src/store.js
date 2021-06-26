29fd2d4cfdaf4018fb9bfb3b0d9b894e5d60a7c3
Nikolay <harnik1988@gmail.com>
2021-06-26 13:25:17 +0300

    enhancer

--- src/store.js
+++ src/store.js
@@ -1,7 +1,31 @@
-import { createStore } from 'redux';
-
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const stringEnhancer = (createStore) => (...args) => { // создаем функцию enhancer в которую передаем createStore который будем изменять и передаем в него его предыдущие свойства
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch; // сохраняем оригинальную функцию store.dispatch
+    store.dispatch = (action) => { 
+        if (typeof action === 'string') {   // это типо мы создали функционал который говорит что наш  dispatch бетет преобразовывать полученные данные под что нам надо
+            return originalDispatch({
+                type: action   // dispatch будет принимать простые строки
+            });
+        }
+        return originalDispatch(action)   // если action не строкаб то нам надо обязательно вернуть  originalDispatch(action) 
+    }
+    return store; // возвращаем измененный store
+};
+
+const logEnhancer = (createStore) => (...args) => { // создаем функцию enhancer в которую передаем createStore который будем изменять и передаем в него его предыдущие свойства
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch; // сохраняем оригинальную функцию store.dispatch
+    store.dispatch = (action) => { 
+        console.log(action.type);
+        return originalDispatch(action)   // нам обязательно надо вернуть originalDispatch(action) в store.dispatch
+    };
+    return store; // возвращаем измененный store
+}
+const store = createStore(reducer, compose ( stringEnhancer, logEnhancer )); // compose - добавляем из библиотеке redux, enhancer добавляется вторым аргументом, что бы в создаваемы store передались новые возможности
+
+store.dispatch('Hello world') 
 
 export default store;
