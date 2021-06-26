531c8aa5e953297e0e79d22556d051b224772063
Nikolay <harnik1988@gmail.com>
2021-06-26 13:52:38 +0300

    middleware

--- src/store.js
+++ src/store.js
@@ -1,30 +1,21 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-const stringEnhancer = (createStore) => (...args) => { // создаем функцию enhancer в которую передаем createStore который будем изменять и передаем в него его предыдущие свойства
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch; // сохраняем оригинальную функцию store.dispatch
-    store.dispatch = (action) => { 
-        if (typeof action === 'string') {   // это типо мы создали функционал который говорит что наш  dispatch бетет преобразовывать полученные данные под что нам надо
-            return originalDispatch({
-                type: action   // dispatch будет принимать простые строки
-            });
-        }
-        return originalDispatch(action)   // если action не строкаб то нам надо обязательно вернуть  originalDispatch(action) 
-    }
-    return store; // возвращаем измененный store
-};
-
-const logEnhancer = (createStore) => (...args) => { // создаем функцию enhancer в которую передаем createStore который будем изменять и передаем в него его предыдущие свойства
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch; // сохраняем оригинальную функцию store.dispatch
-    store.dispatch = (action) => { 
-        console.log(action.type);
-        return originalDispatch(action)   // нам обязательно надо вернуть originalDispatch(action) в store.dispatch
-    };
-    return store; // возвращаем измененный store
+const logMiddleware = ({getState}) => (next)  => (action) => {
+    console.log(action.type, getState());
+    return next(action)
 }
-const store = createStore(reducer, compose ( stringEnhancer, logEnhancer )); // compose - добавляем из библиотеке redux, enhancer добавляется вторым аргументом, что бы в создаваемы store передались новые возможности
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {   
+        return next({
+            type: action   // next это dispatch будет принимать простые строки
+        });
+    }
+    return next(action)
+}
+
+const store = createStore(reducer, applyMiddleware ( stringMiddleware, logMiddleware )); // applyMiddleware - добавляем из библиотеке redux, enhancer добавляется вторым аргументом, что бы в создаваемы store передались новые возможности
 
 store.dispatch('Hello world') 
 
